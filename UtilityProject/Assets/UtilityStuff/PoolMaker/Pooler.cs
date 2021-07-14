937411e4ac5983c177a4251d3742c2784535e06e
Víctor <victorb.belchi18720@gmail.com>
2021-07-14 09:35:22 +0200

    Pooler Maker Class
    A class with static methods for create a pool of objects of type <T> and return the list

---
+++ UtilityProject/Assets/UtilityStuff/PoolMaker/Pooler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Unity;
+using UnityEngine;
+
+public class Pooler : MonoBehaviour {
+
+    public static List<T> GetPoolObjects<T> (T prefab, int numberList, GameObject pool) where T : Component {
+
+        List<T> poolList = new List<T> ();
+
+        for (int i = 0; i < numberList; i++) {
+            T instance = Instantiate (prefab);
+            instance.transform.parent = pool.transform;
+            instance.gameObject.SetActive (false);
+            poolList.Add (instance as T);
+        }
+
+        return poolList;
+
+    }
+
+    public static List<GameObject> GetPoolObjects (GameObject prefab, int numberList, GameObject pool) {
+        List<GameObject> poolList = new List<GameObject> ();
+
+        for (int i = 0; i < numberList; i++) {
+            GameObject instance = Instantiate (prefab);
+            instance.transform.parent = pool.transform;
+            instance.gameObject.SetActive (false);
+            poolList.Add (instance as GameObject);
+        }
+
+        return poolList;
+    }
+}
